4c7f8947ba116c7260a70d9398b20dcb6d46e2ae
Que Te Importa <arnoldd@device-0004.local>
2024-08-22 18:55:16 -0700

    Updated script to inclue Azure API calls to check if storage account exists.

--- stfileupload.sh
+++ stfileupload.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 fileName=""
 storageAccountName=""
 containerName=""
@@ -76,5 +75,35 @@ echo "Name of file to upload: ${fileName}"
 echo "Name of storage account: ${storageAccountName}"
 echo "Name of container: ${containerName}"
 
+
+#Azure API Call
+read -p "Enter your Azure tenants subscription ID: " subscriptionID
+azAPIUrl="https://management.azure.com/subscriptions/${subscriptionID}/providers/Microsoft.Storage/checkNameAvailability?api-version=2023-05-01"
+
+dFlag=$(cat <<EOF
+{
+    "name": "$storageAccountName",
+    "type": "Microsoft.Storage/storageAccounts"
+}
+EOF
+)
+
+accessToken=$(az account get-access-token --query accessToken --output tsv)
+
+jsonResponse=$(curl --request POST "$azAPIUrl" \
+-H "Authorization: Bearer $accessToken" \
+-H "Content-Type: application/json" \
+-d "$dFlag")
+
+nameAvailable=$(echo $jsonResponse | jq '.nameAvailable')
+
+if [ $nameAvailable == 'true' ] 
+then
+    echo "Error: The storage account does not exist."
+    exit 1
+else
+    #logs user into az 
 az login
 az storage blob upload --account-name $storageAccountName --container-name $containerName --file $fileName --verbose
+fi
+
